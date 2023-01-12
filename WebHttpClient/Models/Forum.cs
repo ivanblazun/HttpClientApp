5014ed4e6e125c20c9da40305665c68f2299ae73
Ivan Blažun <ivan.blazun@dignet.hr>
2023-01-12 06:24:53 +0100

    Created dbContext and classes for forum, themes and answers also added relations to user and posts (NO DB UPDATE !)  / IvanB

--- WebHttpClient/Models/Forum.cs
+++ WebHttpClient/Models/Forum.cs
@@ -10,9 +10,9 @@ namespace WebHttpClient.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public int ThemesCounter { get; set; }
+        public int UserCounter { get; set; }
 
         // Navigation props
-        
         public List<Theme> Posts { get; set; }
     }
 }
