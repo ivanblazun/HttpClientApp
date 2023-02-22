f8ee5b02f9d0eabdaa5db61aae23045ec3853465
Ivan Blažun <ivan.blazun@dignet.hr>
2023-02-22 06:16:26 +0100

    Update of all postst from single theme call

---
+++ WebHttpClient/LiveMetrics/LiveMetrics.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.Extensibility.PerfCounterCollector.QuickPulse;
+using System;
+using System.Threading.Tasks;
+
+namespace WebHttpClient.LiveMetrics
+{
+    public class LiveMetrics
+    {
+
+    }
+}
