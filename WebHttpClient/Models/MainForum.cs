f92960af861dd34e162d4ddbaa6bd69914450f04
Ivan Blažun <ivan.blazun@dignet.hr>
2023-03-07 14:20:27 +0100

    Prepared for main forum implement and tine of created added to post, forums, and themes

--- WebHttpClient/Models/MainForum.cs
+++ WebHttpClient/Models/MainForum.cs
@@ -5,18 +5,16 @@ using System.Web;
 
 namespace WebHttpClient.Models
 {
-    public class Forum
+    public class MainForum
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int ThemesCounter { get; set; }
+        public int ForumsCounter { get; set; }
         public int UserCounter { get; set; }
-        public DateTime TimeForumCreated { get; set; }
 
         // Navigation props
 
-        public int MainForumId { get; set; }
-        public MainForum MainForum { get; set; }
-        public List<Theme> Posts { get; set; }
+
+        public List<Forum> Forums { get; set; }
     }
 }
